eec5265914bbb33692fab3549e4cb8f0061db0a6
dc143cDev <devc143c@gmail.com>
2022-11-23 17:38:19 +0900

    fonts

---
+++ lib/palette.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+//색상 팔레트. 테마 변경시 라이트 다크 치환.
+
+const britishRacingGreen = Color(0xFF004225);
+
+const primaryLight = Color(0xFFFDFEFF);
+const lightBg = Color(0xFFDADBDD);
+
+const primaryDark = Color(0xFF16161B);
+const darkBg = Color(0xFF383B39);
